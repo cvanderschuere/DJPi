5737531cf63292e07b05d2a6f19433f556372e2b
cvanderschuere <cdvconcepts@gmail.com>
2013-01-24 18:52:16 -0800

    Initial Commit

---
+++ DJPi/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DJPi
+//
+//  Created by Chris Vanderschuere on 1/24/13.
+//  Copyright (c) 2013 CDVConcepts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DJPi/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DJPi
+//
+//  Created by Chris Vanderschuere on 1/24/13.
+//  Copyright (c) 2013 CDVConcepts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
