ec207d754584a38c64b5c37616bac2fe41cdfdfb
cvanderschuere <cdvconcepts@gmail.com>
2013-03-30 20:59:43 -0700

    Initial test of GAE api; no bugs found

--- DJPI-GAE/main.py
+++ DJPI-GAE/main.py
@@ -15,16 +15,16 @@ class Player(db.Model):
 	title = db.StringProperty()
 	user = db.StringProperty()
 	tracks = db.StringListProperty()
-	
+
 #Useful for rest
 SIMPLE_TYPES = (int, long, float, bool, dict, basestring, list)
 
 def to_dict(model):
     output = {}
-
+    
     for key, prop in model.properties().iteritems():
         value = getattr(model, key)
-
+        
         if value is None or isinstance(value, SIMPLE_TYPES):
             output[key] = value
         elif isinstance(value, datetime.date):
@@ -38,9 +38,9 @@ def to_dict(model):
             output[key] = to_dict(value)
         else:
             raise ValueError('cannot encode ' + repr(prop))
-
+    
     return output
-	
+
 class RestEngine(webapp.RequestHandler):
 	
 	def get(self):
@@ -51,7 +51,7 @@ class RestEngine(webapp.RequestHandler):
 			return #return nothing if no username passed
 		
 		#Handle Players
-		if self.request.path == "/rest/player/":
+		if self.request.path == "/rest/player":
 			self.response.headers['Content-Type'] = "application/json"
 			
 			playerName = self.request.get('title')
@@ -76,7 +76,7 @@ class RestEngine(webapp.RequestHandler):
 					output.append(to_dict(player))
 				
 				self.response.out.write(json.dumps(output))
-				
+		
 		elif self.request.path == "/rest/player/tracks":
 			#Send tracks as appropriate
 			self.response.headers['Content-Type'] = "application/json"
@@ -87,7 +87,7 @@ class RestEngine(webapp.RequestHandler):
 				self.response.set_status(400)
 				return
 			
-			q = Player.gql("WHERE user is :1 AND title is :2",userName,playerTitle)
+			q = Player.gql("WHERE user = :1 AND title = :2",userName,playerTitle)
 			players = q.fetch(limit=1)
 			
 			if len(players) == 0:
@@ -97,81 +97,86 @@ class RestEngine(webapp.RequestHandler):
 			#Output array of tracks
 			self.response.out.write(json.dumps(players[0].tracks))
 			
-			
+	
 	def post(self):
 		#Must pass username
 		userName = self.request.headers['username']
 		if userName == '':
 			self.response.set_status(400)
 			return #return nothing if no username passed
-			
+		
 		if self.request.path == "/rest/player/tracks":
 			#return tracks as appropriate
 			playerName = self.request.get('playerTitle')
 			if playerName == '':
 				self.response.set_status(400)
 				return
+			
+			trackDict = json.loads(self.request.body)
+			
 			#Look if object already exists or not
-			q = Player.gql("WHERE USER IS :1 AND TITLE IS :2",userName,playerName)
+			q = Player.gql("WHERE user = :1 AND title = :2",userName,playerName)
 			players = q.fetch(limit=1)
 			
 			if len(players)==0:
 				self.response.set_status(400)
 				return
-				
-			trackDict = json.loads(self.request.body)
+			
 			
 			for x in trackDict["deletedTracks"]:
 				players[0].tracks.remove(x)
-				
+			
 			for x in trackDict["addedTracks"]:
 				players[0].tracks.append(x)
-				
-			self.response.out.write(json.dumps(players[0].tracks))
 			
-	#Used to create or modify players		
+			self.response.out.write(json.dumps(players[0].tracks))
+	
+	#Used to create or modify players
 	def put(self):
+		
+		userName = self.request.headers['username']
+		if userName == '':
+			self.response.set_status(400)
+			return #return nothing if no username passed
+			
+		self.response.headers['Content-Type'] = "application/json"
+			
 		#PUT player is used to create or update player
 		if self.request.path == "/rest/player":
-		
-			userName = self.request.headers['username']
-			if userName == '':
-				self.response.set_status(400)
-				return #return nothing if no username passed
-				
-			self.response.headers['Content-Type'] = "application/json"
-			
+						
 			#Parse json body
 			playerDict = json.loads(self.request.body)
 			
 			#Look if object already exists or not
-			q = Player.gql("WHERE USER IS :1 AND TITLE IS :2",
-	                              playerDict["user"],playerDict["title"])
-	        players = q.fetch(limit=1)
-	        
+			q = Player.gql("WHERE user = :1 AND title = :2", userName,playerDict["title"])
+			players = q.fetch(limit=1)
+			
 			if len(players)==0:
 				#Create new player with this information
 				newPlayer = Player()
 				newPlayer.title = playerDict["title"]
-				newPlayer.user = playerDict["user"]
-				newPlayer.tracks = []
+				newPlayer.user = userName
+				if self.request.get("replaceTracks") == "YES" or self.request.get("replaceTracks") == "":
+					newPlayer.tracks = playerDict["tracks"]
+				else:
+					newPlayer.tracks = []
 				newPlayer.put()
 				self.response.out.write(json.dumps(to_dict(newPlayer)))
-				
+			
 			else:
 				#Update the first existing player found
 				currentPlayer = players[0]
 				currentPlayer.title = playerDict["title"]
-				currentPlayer.user = playerDict["user"]
+				currentPlayer.user = userName
 				
 				#Change tracks accordingly
 				if self.request.get("replaceTracks") == "YES" or self.request.get("replaceTracks") == "":
 					currentPlayer.tracks = playerDict["tracks"]
-				
+				currentPlayer.put()
 				self.response.out.write(json.dumps(to_dict(currentPlayer)))
-			
 		
 			
+
 application = webapp.WSGIApplication([
 	('/rest/.*', RestEngine)
 ], debug=True)
@@ -181,6 +186,6 @@ def main():
 
 if __name__ == '__main__':
   main()
-	
 
+				
 				
