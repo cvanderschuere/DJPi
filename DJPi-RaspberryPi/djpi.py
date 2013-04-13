0e2d5215f099f1e1105113ca1bc6a7aa23d58978
Chris Vanderschuere <christopher.vanderschuere@gmail.com>
2013-04-13 20:16:21 +0100

    Working version of pi player; Some hacks included

--- DJPi-RaspberryPi/djpi.py
+++ DJPi-RaspberryPi/djpi.py
@@ -7,6 +7,10 @@ import os
 import sys
 import threading
 import time
+import urllib2
+import urllib
+from urllib2 import URLError
+import json
 
 from spotify import Link, SpotifyError
 from spotify.audiosink import import_audio_sink
@@ -17,83 +21,102 @@ AudioSink = import_audio_sink()
 
 class QueueManager(threading.Thread):
 	
-	def __init__(self, player):
+	def __init__(self,player, username):
 		threading.Thread.__init__(self)
+		self.username = "christopher.vanderschuere@gmail.com" #Hack for now
 		self.player = player
-		self.trackQueue = []
+		self.trackQueue = []	
+		self.name = "Kelley"	
 
 	def run(self):
-		print "Started Thread"	
+		print('starting to run')
+		self.updateQueue()
+	
+	def updateQueue(self):
+		print "updating Queue"
+		quotedQuery = urllib.quote(self.name)
+		url = 'http://cdv-djpi.appspot.com/rest/player/tracks?playerTitle=%s' % (quotedQuery)
+	#	try:
+		req = urllib2.Request(url)
+		req.add_header("Content-Type","application/json")
+		req.add_header("username",self.username);
+		r = urllib2.urlopen(req)
+		urls = json.load(r)
 
-		#Load sample queue
-		self.trackQueue = ["spotify:track:64pDOeUGiB0vboqmJx2gGp","spotify:track:2lwwrWVKdf3LR9lbbhnr6R","spotify:track:0yp6ui2sosUjCUro1rRk2Q"]
-		l = Link.from_string(self.trackQueue[0])
-		if not l.type() == Link.LINK_TRACK:
-			print "You can only play tracks!"
-			return
-		self.player.load_track(l.as_track())
-		self.player.play()
+		#Load all new urls
+		self.trackQueue = urls;
 
-class PiPlayer(SpotifySessionManager):
+		if not self.player.playing and not self.player.loading:
+			self.player.next()
+	
+			
+		threading.Timer(10,self.updateQueue).start()	
+	#	except URLError as e:
+	#		print e.reason + str(e.code)
 
-	queued = False
+
+#Run the manager on a seperate thread
+class SpotifyManager(SpotifySessionManager):
+
+	queued = True
 	playlist = 2
 	track = 0	
 	appkey_file = os.path.join(os.path.dirname(__file__), 'spotify_appkey.key')
 
-	def __init__(self, *a, **kw):
-		SpotifySessionManager.__init__(self, *a, **kw)
+	def __init__(self,username, *a, **kw):
+		SpotifySessionManager.__init__(self, username,*a, **kw)
 		self.audio = AudioSink(backend=self)
-		self.manager = QueueManager(self)
 		self.playing = False
-		self.trackQueue = []
-		self.currentIndex = 0;
+		self._queue = []
+		self.currentIndex = 0
 		self.track_playing = None
-		print "Logging in, please wait..."
-
+		self.manager = QueueManager(self,username)
+		self.loading = False			
 
 	def logged_in(self, session, error):
 		if error:
-			print error
+			print(error)
 			return
-		print "Logged in!"
+		print("Logged in!")
+		self.manager.start()
 		
-		if not self.manager.is_alive():
-			self.manager.start()
-	
+
 	def logged_out(self, session):
-		print "Logged out!"
+		print("Logged out!")
 	
 	def load_track(self, track):
-		print u"Loading track..."
+		print(u"Loading track...")
+		self.loading = True
 		while not track.is_loaded():
 			time.sleep(1)
 		
 		if track.is_autolinked(): # if linked, load the target track instead
-			print "Autolinked track, loading the linked-to track"
-	   		return self.load_track(track.playable())
+			print("Autolinked track, loading the linked-to track")
+			return self.load_track(track.playable())
 		
 		if track.availability() != 1:
-   			print "Track not available (%s)" % track.availability()
+   			print("Track not available (%s)" % track.availability())
 		
 		if self.playing:
    			self.stop()
 		self.new_track_playing(track)
 		self.session.load(track)
-		print "Loaded track: %s" % track.name()
-	
+		print("Loaded track: %s" % track.name())
+		self.play()	
+		self.loading = False
+		
 	def new_track_playing(self, track):
 		self.track_playing = track
 	
 	def play(self):
 	   self.audio.start()
 	   self.session.play(1)
-	   print "Playing"
+	   print("Playing")
 	   self.playing = True
 	
 	def stop(self):
 	   self.session.play(0)
-	   print "Stopping"
+	   print("Stopping")
 	   self.playing = False
 	   self.audio.stop()
 	
@@ -101,52 +124,37 @@ class PiPlayer(SpotifySessionManager):
 	   return self.audio.music_delivery(*args, **kwargs)
 	
 	def next(self):
-	   self.stop()
-	   if self._queue:
-	       t = self._queue.pop(0)
-	       self.load(*t)
-	       self.play()
-	   else:
-	       self.stop()
+		if self.playing:
+			self.stop()
+		if self.manager.trackQueue:
+			if len(self.manager.trackQueue) > self.currentIndex:
+				l = Link.from_string(self.manager.trackQueue[self.currentIndex])	
+				self.currentIndex += 1
+				print self.currentIndex
+				if l.type() == Link.LINK_TRACK:
+					threading.Timer(0,self.load_track,[l.as_track()]).start()
+				else:
+					self.stop()
+			else:
+				print "Reached end of queue"
+				self.stop()
 	
 	def end_of_track(self, sess):
+		print("End of track")
 		self.audio.end_of_track()
-
-		#Move to next track in queue
-		self.currentIndex += 1
-		if self.currentIndex < len(self.manager.trackQueue):
-			l = Link.from_string(self.manager.trackQueue[self.currentIndex])
-			if not l.type() == Link.LINK_TRACK:
-				print "You can only play tracks!"
-				return
-			self.load_track(l.as_track())
-
-		#Update queue from webserver
-
-## container calllbacks ##
-class JukeboxContainerManager(SpotifyContainerManager):
-    def container_loaded(self, c, u):
-        container_loaded.set()
-
-    def playlist_added(self, c, p, i, u):
-        print 'Container: playlist "%s" added.' % p.name()
-
-    def playlist_moved(self, c, p, oi, ni, u):
-        print 'Container: playlist "%s" moved.' % p.name()
-
-    def playlist_removed(self, c, p, i, u):
-        print 'Container: playlist "%s" removed.' % p.name()
+		
 
 
 if __name__ == '__main__':
-    import optparse
-    op = optparse.OptionParser(version="%prog 0.1")
-    op.add_option("-u", "--username", help="Spotify username")
-    op.add_option("-p", "--password", help="Spotify password")
-    op.add_option("-v", "--verbose", help="Show debug information",
-        dest="verbose", action="store_true")
-    (options, args) = op.parse_args()
-    if options.verbose:
-        logging.basicConfig(level=logging.DEBUG)
-    session_m = PiPlayer(options.username, options.password, True)
-    session_m.connect()
+	import optparse
+	op = optparse.OptionParser(version="%prog 0.1")
+	op.add_option("-u", "--username", help="Spotify username")
+	op.add_option("-p", "--password", help="Spotify password")
+	op.add_option("-v", "--verbose", help="Show debug information",
+	dest="verbose", action="store_true")
+	(options, args) = op.parse_args()
+	if options.verbose:
+		logging.basicConfig(level=logging.DEBUG)
+
+	player = SpotifyManager(options.username,options.password,True);
+	player.connect()
